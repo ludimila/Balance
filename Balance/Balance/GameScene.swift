bf474cbc32796a7f68fd88170079303682cf5efe
wsickert <wesley.sickert@gmail.com>
2016-05-12 14:51:49 -0300

    criado timer de inicio do jogo

--- Balance/Balance/GameScene.swift
+++ Balance/Balance/GameScene.swift
@@ -9,12 +9,38 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var timer: NSTimer!
+    var time:Int = 3
+    var ascending = false
+    var actionTimeGame = SKAction()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+    
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
         myLabel.fontSize = 65
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        let timer = SKAction.waitForDuration(1)
+        self.actionTimeGame = SKAction.runBlock {
+            if self.time <= 3 && self.ascending == false {
+                if self.time == 0 {
+                    myLabel.text = "GO!"
+                    self.ascending = true
+                    self.time += 1
+                }else {
+                    myLabel.text = "\(self.time)"
+                    self.time -= 1
+                }
+            }else {
+                myLabel.text = "\(self.time)"
+                self.time += 1
+            }
+        }
+        
+        self.runAction(SKAction.repeatActionForever(SKAction.sequence([actionTimeGame, timer])),withKey: "timer")
         
         self.addChild(myLabel)
     }
@@ -41,5 +67,9 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func startGame() {
+        print("Funcionou startGame")
     }
 }
