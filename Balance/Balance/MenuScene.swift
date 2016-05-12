f395b1ebd547a278d2517c0631b521ac524d8542
Judar Lima <judar.rslima@gmail.com>
2016-05-12 20:26:41 -0300

    Ajuste do botão de play

--- Balance/Balance/MenuScene.swift
+++ Balance/Balance/MenuScene.swift
@@ -14,21 +14,14 @@ class MenuScene: SKScene {
     var backgroundLayer: BackgroundLayer!
     
     override func didMoveToView(view: SKView) {
-        playButton = SKSpriteNode(color: SKColor.redColor(), size: CGSize(width: 100, height: 40))
-        
-        playButton.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-        
+        playButton = SKSpriteNode(texture: SKTexture(imageNamed: "play"), color: UIColor.clearColor(), size: CGSizeMake(300, 100))
+        playButton.position = CGPoint(x: CGRectGetMidX(self.frame), y: self.frame.height/2)
+        playButton.zPosition = 3
         self.addChild(playButton)
         
-        let playLabel = SKLabelNode(fontNamed: "Chalkduster")
-        playLabel.text = "Play!"
-        playLabel.fontSize = 35
-        playLabel.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) - playLabel.frame.size.height/2)
-        playLabel.fontColor = UIColor.greenColor()
-        
-        self.addChild(playLabel)
         
         self.backgroundLayer = BackgroundLayer(size: size)
+        self.backgroundLayer.zPosition = -2
         self.addChild(self.backgroundLayer)
         
     }
